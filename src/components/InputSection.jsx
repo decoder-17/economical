d3bc7ba226480765f5c2e2494dab9795e3eb4979
coder_17 <tanupamsaha26@gmail.com>
2023-06-24 12:17:03 +0530

    Updated: Mainframe.

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -10,22 +10,24 @@ export default function InputSection() {
   };
   return (
     <div className="">
-      <div className="font-mono bg-white min-h-full flex justify-center align-middle">
+      <div className="font-mono bg-white min-h-full flex justify-center align-middle rounded-2xl">
         <div className="container mx-auto">
+          <h3 className="mt-4 pt-4 text-2xl text-center">
+            Confused of which scheme to choose?
+          </h3>
+
+          <h5 className="mt-4 text-xl text-center">
+            Think no more, got you covered!
+          </h5>
           <div className="flex justify-center px-6 my-12">
             <div className="w-full xl:w-3/4 lg:w-11/12 flex">
               <div
                 className="w-full h-auto  hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
                 style={style}
               ></div>
-
               <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
-                <h3 className="pt-4 text-2xl text-center">
-                  Confused of which scheme to choose?
-                </h3>
-                <h5 className="pt-4 text-xl text-center">
-                  Think no more, got you covered!
-                </h5>
+                <h3 className="text-2xl text-center">Fill in the details!</h3>
+
                 <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
                   <div className="mb-4 md:flex md:justify-between">
                     <div className="mb-4 md:mr-2 md:mb-0">
@@ -54,6 +56,36 @@ export default function InputSection() {
                         id="lastName"
                         type="text"
                         placeholder="Last Name"
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-4 md:flex md:justify-between">
+                    <div className="mb-4 md:mr-2 md:mb-0">
+                      <label
+                        className="block mb-2 text-sm font-bold text-gray-700"
+                        htmlFor="firstName"
+                      >
+                        Role
+                      </label>
+                      <input
+                        className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        id="firstName"
+                        type="text"
+                        placeholder="Role"
+                      />
+                    </div>
+                    <div className="md:ml-2">
+                      <label
+                        className="block mb-2 text-sm font-bold text-gray-700"
+                        htmlFor="lastName"
+                      >
+                        Location
+                      </label>
+                      <input
+                        className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                        id="lastName"
+                        type="text"
+                        placeholder="Location"
                       />
                     </div>
                   </div>
@@ -123,10 +155,10 @@ export default function InputSection() {
                       class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
                       type="button"
                     >
-                      Submit
+                      Calculate
                     </button>
                   </div>
-                  <hr class="mb-6 border-t" />
+                  {/* <hr class="mb-6 border-t" /> */}
                 </form>
               </div>
             </div>
